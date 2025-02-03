c5926716f5c3a61851b0f651978336cdb0f1a920
niksidbro <nk521760@gmail.com>
2025-02-03 18:53:18 +0530

    Update snap.js

--- snap.js
+++ snap.js
@@ -1,9 +1,9 @@
 document.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
-        document.body.classList.add("tornado"); // Tornado effect
+        document.body.classList.add("pixelate"); // Pixelate effect
 
         setTimeout(function() {
             window.location.href = "https://www.google.com";
-        }, 1200);
+        }, 1000);
     }
 });
