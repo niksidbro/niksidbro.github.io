a41553f1e4c683a54b561f922d96a3bf507a03c8
niksidbro <nk521760@gmail.com>
2025-02-17 09:16:03 +0530

    Update and rename copyright.js to scroll2top.js

--- copyright.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const year = new Date().getFullYear();
-    const siteName = window.location.hostname;
-    const siteURL = window.location.origin;
-
-    document.getElementById("copyright").innerHTML = 
-        `&copy; ${year} <a href="${siteURL}" target="_blank">${siteName}</a>. All rights reserved.`;
-});
