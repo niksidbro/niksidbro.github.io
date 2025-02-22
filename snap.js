cb9190cc11399e301ac398e1586b913f126a7c70
niksidbro <nk521760@gmail.com>
2025-02-22 16:37:49 +0530

    Delete snap.js

--- snap.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("keydown", function(event) {
-    if (event.key === "Escape") {
-        document.body.classList.add("pixelate"); // Pixelate effect
-
-        setTimeout(function() {
-            window.location.href = "https://www.google.com";
-        }, 1000);
-    }
-});
