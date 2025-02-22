35c9c662bb9b7923f67520cb870ce81b841b2624
niksidbro <nk521760@gmail.com>
2025-02-22 23:27:15 +0530

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,9 +1,9 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-NWG85NQ1SP"></script>
-<script>
+async src="https://www.googletagmanager.com/gtag/js?id=G-NWG85NQ1SP"
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-NWG85NQ1SP');
-</script>
+
