b17a68d441fb50cd7cb85189b7f50bddc7f97d42
niksidbro <nk521760@gmail.com>
2025-02-22 23:22:16 +0530

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NWG85NQ1SP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NWG85NQ1SP');
+</script>
