87272d297650f72883e6af261efc508ba27b9769
niksidbro <nk521760@gmail.com>
2025-02-22 16:36:59 +0530

    Delete loading.js

--- loading.js
+++
@@ -1,7 +0,0 @@
-   const iframe = document.getElementById("gameFrame");
-        const loading = document.getElementById("loading");
-
-        iframe.onload = function () {
-            loading.style.display = "none"; // Hide loading text
-            iframe.style.display = "block"; // Show iframe
-        };
